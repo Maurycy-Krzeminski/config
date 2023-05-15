09e6ed8ce5af5c357f2e7453cb906a0e03878f0e
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-05-15 22:07:11 +0200

    formatting

--- lua/moris/set.lua
+++ lua/moris/set.lua
@@ -1,20 +1,20 @@
---vim.opt.guicursor = ""
+local opt = vim.opt
+--opt.guicursor = ""
 
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.opt.smartindent = true
-
-vim.opt.wrap = false
-
-
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+opt.nu = true -- Print line number
+opt.relativenumber = true -- Relative line numbers
+opt.tabstop = 4 -- Number of spaces tabs count for
+opt.softtabstop = 4
+opt.shiftwidth = 4 -- Size of an indent
+opt.expandtab = true -- Use spaces instead of tabs
+opt.smartindent = true -- Insert indents automatically
+opt.wrap = false
+opt.hlsearch = false
+opt.incsearch = true
+opt.scrolloff = 8
+opt.signcolumn = "yes"
+opt.isfname:append("@-@")
+--opt.colorcolumn = "80"
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
@@ -22,9 +22,6 @@ vim.o.completeopt = 'menuone,noselect'
 -- NOTE: You should make sure your terminal supports this
 vim.o.termguicolors = true
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
 
 -- Decrease update time
 vim.o.updatetime = 250
@@ -35,5 +32,4 @@ vim.o.timeoutlen = 300
 -- Save undo history
 vim.o.undofile = true
 
---vim.opt.colorcolumn = "80"
 
