eba58b6c6248d995bb39daa4d00a86956272ddf1
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-06-13 21:07:35 +0200

    Java, Kotlin added, lsp keymap changed

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -34,6 +34,8 @@ return{
             'tsserver',
             'rust_analyzer',
             'lua_ls',
+            'kotlin_language_server',
+            'jdtls',
         })
 
         -- Fix Undefined global 'vim'
@@ -75,9 +77,9 @@ return{
             vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
             vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
             vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-            vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+            vim.keymap.set("n", "<leader>va", function() vim.lsp.buf.code_action() end, opts)
+            vim.keymap.set("n", "<leader>vr", function() vim.lsp.buf.references() end, opts)
+            vim.keymap.set("n", "<leader>vn", function() vim.lsp.buf.rename() end, opts)
             vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
         end)
 
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,4 @@
-return {
+return{{ "MunifTanjim/nui.nvim", lazy = true }, {
     "nvim-treesitter/nvim-treesitter",
 
   dependencies = {
@@ -18,7 +18,7 @@ return {
 
     configs.setup {
 	    -- A list of parser names, or "all"
-	    ensure_installed = { "vimdoc", "javascript", "typescript", "lua", "rust", "go", "java", "kotlin", "svelte" },
+	    ensure_installed = { "vimdoc", "javascript", "typescript", "lua", "rust", "go", "svelte" },
 
 	    -- Install parsers synchronously (only applied to `ensure_installed`)
 	    sync_install = false,
@@ -31,12 +31,13 @@ return {
 		    -- `false` will disable the whole extension
 		    enable = true,
 
-		    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-		    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-		    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-		    -- Instead of true it can also be a list of languages
-		    additional_vim_regex_highlighting = false,
-	    },
+            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+            -- Using this option may slow down your editor, and you may see some duplicate highlights.
+            -- Instead of true it can also be a list of languages
+            additional_vim_regex_highlighting = false,
+        },
     }
-  end
+end
+}
 }
