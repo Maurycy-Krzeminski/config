fc2fbdeb11fa4e7c204adbcb35c6607b99c506a0
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-06-25 19:40:19 +0200

    telescope search git

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return{
 		--local opts = { silent = true }
 		local opts = {}
 		keymap("n", "<leader>ff", ":Telescope find_files<CR>", opts)
+		keymap("n", "<leader>fg", ":Telescope git_files<CR>", opts)
 		keymap("n", "<C-p>", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
 		end, opts)
