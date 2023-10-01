c0e2dc294f9ec1add8eb5f544d9d66076d0f8d18
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-10-01 11:17:20 +0200

    Descriptions for lsp keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -73,15 +73,25 @@ return{
         lsp.on_attach(function(client, bufnr)
             local opts = {buffer = bufnr, remap = false}
             local keymap = vim.keymap.set
+            opts.desc = "got to definition"
             keymap("n", "gd", function() vim.lsp.buf.definition() end, opts)
+            opts.desc = "hover"
             keymap("n", "K", function() vim.lsp.buf.hover() end, opts)
+            opts.desc = "workspace symbol"
             keymap("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+            opts.desc = "open float"
             keymap("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+            opts.desc = "got to next"
             keymap("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+            opts.desc = "got to prev"
             keymap("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+            opts.desc = "code action"
             keymap("n", "<leader>va", function() vim.lsp.buf.code_action() end, opts)
+            opts.desc = "references"
             keymap("n", "<leader>vr", function() vim.lsp.buf.references() end, opts)
+            opts.desc = "rename"
             keymap("n", "<leader>vn", function() vim.lsp.buf.rename() end, opts)
+            opts.desc = "signature help"
             keymap("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
         end)
         lsp.skip_server_setup({'jdtls'})
