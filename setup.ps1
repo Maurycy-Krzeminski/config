f935ee0aef077775758e6d98716f73f6dc532570
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-10-01 11:51:14 +0200

    Setup state config

--- setup.ps1
+++ setup.ps1
@@ -1,1 +1,8 @@
-[Environment]::SetEnvironmentVariable('XDG_CONFIG_HOME',$pwd.path)
+$configPath = $pwd.path
+echo 'configPath:'$configPath  
+[Environment]::SetEnvironmentVariable('XDG_CONFIG_HOME',$configPath)
+$statePath = Join-Path -Path $configPath -ChildPath "data"
+echo 'statePath:'$statePath
+[Environment]::SetEnvironmentVariable('XDG_STATE_HOME',$statePath)
+
+
