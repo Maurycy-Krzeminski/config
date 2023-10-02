76a077830c564aed58089333906dcd2ebed85864
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-10-02 21:47:03 +0200

    XDG_DATA_HOME setup

--- setup.ps1
+++ setup.ps1
@@ -1,9 +1,11 @@
 $configPath = $pwd.path
 echo 'configPath:'$configPath  
 [Environment]::SetEnvironmentVariable('XDG_CONFIG_HOME',$configPath)
+
 $statePath = Join-Path -Path $configPath -ChildPath "data"
-echo 'statePath:'$statePath
+echo 'statePath data:'$statePath
 [Environment]::SetEnvironmentVariable('XDG_STATE_HOME',$statePath)
+[Environment]::SetEnvironmentVariable('XDG_DATA_HOME',$statePath)
 
 $npmPath = Join-Path -Path $configPath -ChildPath "npm" | Join-Path -ChildPath ".npmrc"
 echo 'npmPath:'$npmPath
