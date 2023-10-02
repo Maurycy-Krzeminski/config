dc049089d94d8d3f641b3143eeb0de0359b4018c
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-10-02 20:07:11 +0200

    NPMRC setup

--- setup.ps1
+++ setup.ps1
@@ -5,4 +5,7 @@ $statePath = Join-Path -Path $configPath -ChildPath "data"
 echo 'statePath:'$statePath
 [Environment]::SetEnvironmentVariable('XDG_STATE_HOME',$statePath)
 
+$npmPath = Join-Path -Path $configPath -ChildPath "npm" | Join-Path -ChildPath ".npmrc"
+echo 'npmPath:'$npmPath
+[Environment]::SetEnvironmentVariable('NPM_CONFIG_USERCONFIG',$npmPath)
 
