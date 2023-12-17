309e59485711032acf23bdfe17e4027b3f6745ad
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-12-17 16:47:58 +0100

    Harpoon cleanup

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -5,18 +5,18 @@ return {
         "nvim-lua/plenary.nvim",
     },
     config = function()
-        local keymap = vim.keymap.set
         local harpoon = require("harpoon")
         -- REQUIRED
         harpoon:setup()
 
+        -- KEYMAPS
+        local keymap = vim.keymap.set
         keymap("n", "<leader>a", function() harpoon:list():append() end, {desc = "add file to harpoon"})
         keymap("n", "<C-q>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, {desc = "Harpoon quick menu"})
-
-        keymap("n", "<C-a>", function() harpoon:list():select(1) end)
-        keymap("n", "<C-s>", function() harpoon:list():select(2) end)
-        keymap("n", "<C-d>", function() harpoon:list():select(3) end)
-        keymap("n", "<C-f>", function() harpoon:list():select(4) end)
+        keymap("n", "<C-a>", function() harpoon:list():select(1) end, {desc = "Harpoon select 1"})
+        keymap("n", "<C-s>", function() harpoon:list():select(2) end, {desc = "Harpoon select 2"})
+        keymap("n", "<C-d>", function() harpoon:list():select(3) end, {desc = "Harpoon select 3"})
+        keymap("n", "<C-f>", function() harpoon:list():select(4) end, {desc = "Harpoon select 4"})
 
 
 	end
