8016d50ab9471bb2efea5da5f795b20b141429a1
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-12-20 21:37:05 +0100

    Util to print tables

--- nvim/lua/plugins/java.lua
+++ nvim/lua/plugins/java.lua
@@ -1,4 +1,14 @@
 return {
+  -- Add java to treesitter.
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "java" })
+    end,
+  },
+
+    {
     "nvim-java/nvim-java",
     dependencies = {
         "nvim-java/lua-async-await",
@@ -29,4 +39,4 @@ return {
         },
     },
     opts = {},
-}
+}}
