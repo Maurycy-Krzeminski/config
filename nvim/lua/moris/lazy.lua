2ae9fd12717889c13e24c0ee03c234bcbef3ddbd
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2023-12-24 02:04:51 +0100

    lazy cleanup

--- nvim/lua/moris/lazy.lua
+++ nvim/lua/moris/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins",{
+require("lazy").setup({
     spec = {
         { import = "plugins" },
         { import = "plugins.langs" },
