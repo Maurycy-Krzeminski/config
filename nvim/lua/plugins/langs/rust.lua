61ffbcad0ed939c86c54d3d18a9e4d70066da4cc
Maurycy-Krzeminski <maurycy.krzeminski@gmail.com>
2024-01-15 20:06:28 +0100

    Rust additional plugins

--- nvim/lua/plugins/langs/rust.lua
+++ nvim/lua/plugins/langs/rust.lua
@@ -12,6 +12,21 @@ return {
         end,
     },
     {
-
+        'mrcjkb/rustaceanvim',
+        version = '^3', -- Recommended
+        ft = { 'rust' },
     },
+    {
+        "mfussenegger/nvim-dap",
+        optional = true,
+        dependencies = {
+            {
+                "williamboman/mason.nvim",
+                opts = function(_, opts)
+                    opts.ensure_installed = opts.ensure_installed or {}
+                    vim.list_extend(opts.ensure_installed, { "codelldb" })
+                end,
+            },
+        },
+    }
 }
